ea5dd006461e252189917d149a1abb6d860fd167
bermanjacob96 <bermanjacob96@gmail.com>
2019-06-24 14:46:45 -0600

    Add Model Class and Add/Register db context
    - Removes unused using declarations.
    - Adds the database context to the DI container.
    - Specifies that the database context will use an in-memory database.

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models
+{
+    public class TodoApi
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool isDone { get; set; }
+        
+    }
+}
